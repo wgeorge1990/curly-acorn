006e6e5fd40d0bbd32d0e79636192a66e2a1ec47
William George <42682665+wgeorge1990@users.noreply.github.com>
2023-04-22 21:01:23 +0000

    simplify

--- src/Modal.js
+++ src/Modal.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-export default function Modal(props) {
+export default function Modal({text, modalHandler}) {
     return <div className="card m-2">
                 <div className="header">
                     <div className="btn btn-primary p-2 m-2"
-                        onClick={props.modalHandler}>
+                        onClick={modalHandler}>
                         close
                     </div>
                     <div className="centered m-2">
-                        <strong>{props.text}</strong>
+                        <strong>{text}</strong>
                     </div>
                 </div>
             </div>
